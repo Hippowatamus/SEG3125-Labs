4ab536ba3822ef7449a6e682abfa1fdbe6d62381
Hippowatamus <colinpeters@rogers.com>
2019-06-04 22:59:28 -0400

    added new menu pages, started navigation and JS

---
+++ script.js
@@ -0,0 +1,19 @@
+function navToWelcomePage() {
+    window.location.href = "WelcomePage.html";
+}
+
+function navToCheckoutPage() {
+    window.location.href = "CheckoutPage.html";
+}
+
+function navToListofRestaurants() {
+    window.location.href = "ListofRestaurants.html";
+}
+
+function navToMenuPage(menuPage) {
+    window.location.href = menuPage;
+}
+
+function navToOrderPage() {
+    window.location.href = "OrderPage.html";
+}
